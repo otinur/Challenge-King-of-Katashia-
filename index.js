78c442014d1c90ed9f827a9283375576b81c9776
otinur-rahman <otinurrhman@gmail.com>
2024-02-13 18:51:54 +0600

    a

---
+++ index.js
@@ -0,0 +1,9 @@
+// const menuIcon =document.getElementById("menu-icon")
+//  const menu =document.getElementById("menu")
+//  menuIcon.addEventListener("click",()=>{
+//      if(menu.className == "nav-bar"){
+//          menu.classList.remove("nav-bar");
+//      }else{
+//          menu.classList.add("nav-bar");
+//      }
+//  })
